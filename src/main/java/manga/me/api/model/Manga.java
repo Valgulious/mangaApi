472bd010fa7a3c6148f59922314a025980e945a6
Valgulious <dmitryfedorenkof@gmail.com>
2019-06-03 19:14:57 +1000

    Revert "Revert "first commit""
    This reverts commit 0b436de074cb65133b585f57f6a545b2c0555307.

---
+++ src/main/java/manga/me/api/model/Manga.java
@@ -0,0 +1,80 @@
+package manga.me.api.model;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Document
+public class Manga {
+
+    @Id
+    private String id;
+
+    private String russianTitle;
+    private String englishTitle;
+    private String author;
+    private String description;
+    private Status status;
+
+    public Manga() {
+    }
+
+    public Manga(String russianTitle, String englishTitle, String author, String description, Status status) {
+        this.russianTitle = russianTitle;
+        this.englishTitle = englishTitle;
+        this.author = author;
+        this.description = description;
+        this.status = status;
+    }
+
+    public String getRussianTitle() {
+        return russianTitle;
+    }
+
+    public void setRussianTitle(String russianTitle) {
+        this.russianTitle = russianTitle;
+    }
+
+    public String getEnglishTitle() {
+        return englishTitle;
+    }
+
+    public void setEnglishTitle(String englishTitle) {
+        this.englishTitle = englishTitle;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Status getStatus() {
+        return status;
+    }
+
+    public void setStatus(Status status) {
+        this.status = status;
+    }
+
+    @Override
+    public String toString() {
+        return "Manga{" +
+                "id='" + id + '\'' +
+                ", russianTitle='" + russianTitle + '\'' +
+                ", englishTitle='" + englishTitle + '\'' +
+                ", author='" + author + '\'' +
+                ", description='" + description + '\'' +
+                ", status='" + status + '\'' +
+                '}';
+    }
+}
